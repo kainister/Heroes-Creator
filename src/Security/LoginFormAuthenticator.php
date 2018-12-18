27fb3b879b2a094d7c0ed2f27b635a2a1cfeb3b2
Kevin Badinca <kevin.badinca@supinternet.fr>
2018-12-18 18:36:21 +0100

    Added login and logout path

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -85,7 +85,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-         return new RedirectResponse($this->router->generate('home'));
+         return new RedirectResponse($this->router->generate('admin.hero.index'));
     }
 
     protected function getLoginUrl()
